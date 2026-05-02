30403f93c10732133605ecce60fb21f2ff08b0b2
ayesha314 <ayesha.khalid182005@gmail.com>
2026-05-02 11:39:01 +0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -12,105 +12,38 @@ protected:
     int balance;
     vector<Card> hand;
     bool isFolded;
-    bool isSeen;          // Blind / Seen system later
+    bool isSeen;
 
 public:
     // Constructor
-    Player(string n, int b = 1000)
-        : name(n), balance(b), isFolded(false), isSeen(false) {}
+    Player(string n, int b = 1000);
 
     // Virtual destructor
-    virtual ~Player() {}
+    virtual ~Player();
 
-    // Pure virtual function (Polymorphism)
+    // Pure virtual function
     virtual void takeTurn() = 0;
 
-    // Add card to hand
-    void addCard(const Card& c) {
-        if (hand.size() < 3) {
-            hand.push_back(c);
-        }
-    }
+    // Card functions
+    void addCard(const Card& c);
+    void clearHand();
+    void printHand() const;
+    vector<Card> getHand() const;
 
-    // Clear hand for new round
-    void clearHand() {
-        hand.clear();
-        isFolded = false;
-        isSeen = false;
-    }
-
-    // Display cards
-    void printHand() const {
-        cout << "\n" << name << "'s Hand:\n";
-        cout << "-------------------\n";
-
-        for (int i = 0; i < hand.size(); i++) {
-            cout << "[" << i + 1 << "] "
-                 << hand[i].toString() << endl;
-        }
-
-        cout << "-------------------\n";
-    }
-
-    // Get hand vector
-    vector<Card> getHand() const {
-        return hand;
-    }
-
-    // Fold logic
-    void fold() {
-        isFolded = true;
-        cout << name << " folded.\n";
-    }
-
-    // Betting
-    void bet(int amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            cout << name << " bets $" << amount << endl;
-        }
-        else {
-            cout << name << " doesn't have enough chips.\n";
-        }
-    }
-
-    // See cards
-    void seeCards() {
-        isSeen = true;
-        cout << name << " has seen their cards.\n";
-    }
+    // Game actions
+    void fold();
+    void bet(int amount);
+    void seeCards();
 
     // Getters
-    string getName() const {
-        return name;
-    }
+    string getName() const;
+    int getBalance() const;
+    bool getIsFolded() const;
+    bool getIsSeen() const;
 
-    int getBalance() const {
-        return balance;
-    }
+    // Money
+    void addBalance(int amount);
 
-    bool getIsFolded() const {
-        return isFolded;
-    }
-
-    bool getIsSeen() const {
-        return isSeen;
-    }
-
-    // Add chips when winning
-    void addBalance(int amount) {
-        balance += amount;
-    }
-
-    // Hand ranking helper
-    int getHighestCard() const {
-        int highest = 0;
-
-        for (const Card& c : hand) {
-            if (c.getRank() > highest)
-                highest = c.getRank();
-        }
-
-        return highest;
-    }
+    // Hand helper
+    int getHighestCard() const;
 };
