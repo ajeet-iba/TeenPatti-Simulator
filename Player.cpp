6f2e29212debbc8c8140e5a5704ee81e708e7e6b
ayesha314 <ayesha.khalid182005@gmail.com>
2026-05-02 11:38:21 +0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,25 +1,87 @@
 #include "Player.h"
 #include <iostream>
 
-Player::Player(std::string name, int balance)
-    : name(name), balance(balance), isFolded(false) {}
+Player::Player(string n, int b)
+    : name(n), balance(b), isFolded(false), isSeen(false) {}
 
-void Player::addCard(Card c) {
-    hand.push_back(c);
+Player::~Player() {}
+
+void Player::addCard(const Card& c) {
+    if (hand.size() < 3) {
+        hand.push_back(c);
+    }
+}
+
+void Player::clearHand() {
+    hand.clear();
+    isFolded = false;
+    isSeen = false;
 }
 
 void Player::printHand() const {
-    std::cout << name << "'s hand:\n";
-    for (int i = 0; i < (int)hand.size(); ++i) {
-        std::cout << "  [" << (i + 1) << "] " << hand[i].toString() << "\n";
+    cout << "\n" << name << "'s Hand:\n";
+    cout << "-------------------\n";
+
+    for (int i = 0; i < hand.size(); i++) {
+        cout << "[" << i + 1 << "] "
+             << hand[i].toString() << endl;
     }
+
+    cout << "-------------------\n";
+}
+
+vector<Card> Player::getHand() const {
+    return hand;
 }
 
 void Player::fold() {
     isFolded = true;
-    std::cout << name << " has folded.\n";
+    cout << name << " folded.\n";
 }
 
-std::string Player::getName() const { return name; }
-int Player::getBalance() const     { return balance; }
-bool Player::getIsFolded() const   { return isFolded; }
+void Player::bet(int amount) {
+    if (amount <= balance) {
+        balance -= amount;
+        cout << name << " bets $" << amount << endl;
+    }
+    else {
+        cout << name << " doesn't have enough chips.\n";
+    }
+}
+
+void Player::seeCards() {
+    isSeen = true;
+    cout << name << " has seen their cards.\n";
+}
+
+string Player::getName() const {
+    return name;
+}
+
+int Player::getBalance() const {
+    return balance;
+}
+
+bool Player::getIsFolded() const {
+    return isFolded;
+}
+
+bool Player::getIsSeen() const {
+    return isSeen;
+}
+
+void Player::addBalance(int amount) {
+    balance += amount;
+}
+
+int Player::getHighestCard() const {
+    int highest = 0;
+
+    for (const Card& c : hand) {
+        if (c.getRank() > highest) {
+            highest = c.getRank();
+        }
+    }
+
+    return highest;
+}
